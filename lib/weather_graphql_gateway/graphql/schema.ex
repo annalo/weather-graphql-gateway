25d7749e90fae7fcdc952939ed1e2ea84f2355e8
Anna Lo <annayylo@gmail.com>
2024-04-02 18:51:31 -0400

    delete schema from graphql

--- lib/weather_graphql_gateway/graphql/schema.ex
+++
@@ -1,9 +0,0 @@
-defmodule WeatherGraphqlGateway.Graphql.Schema do
-  use Absinthe.Schema
-
-  alias WeatherGraphqlGateway.NewsResolver
-
-  query do
-    # this is the query entry point to our app
-  end
-end
