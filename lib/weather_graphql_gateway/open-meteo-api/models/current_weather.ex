d37ce4122255b88da9296142fa643e29b6622605
Anna Lo <annayylo@gmail.com>
2024-04-04 15:47:57 -0400

    add time to current weather model

--- lib/weather_graphql_gateway/open-meteo-api/models/current_weather.ex
+++ lib/weather_graphql_gateway/open-meteo-api/models/current_weather.ex
@@ -11,16 +11,20 @@ defmodule WeatherGraphqlGateway.OpenMeteoAPI.Models.CurrentWeather do
   - `is_day`: 1 if the current time step has daylight, 0 at night.
   - `precipitation`: Total current precipitation (rain, showers, snow) sum. (mm/inch)
   - `relative_humidity_2m`: The relative humidity at 2 meters above ground level. (in percentage)
+  - `time`: Time the temperature was taken. 
   - `temperature_2m`: The air temperature at 2 meters above ground level. (C/F)
   - `weather_code`: Weather condition as a numeric code. Follow WMO weather interpretation codes.
   - `wind_speed_10m`: The wind speed at 10 meters above ground level. 10 meters is the standard level. (km/h, mph, m/s, knots)
 
 """
+  @enforce_keys []
   defstruct [
     :apparent_temperature,
+    :cloud_cover,
     :is_day,
     :precipitation,
     :relative_humidity_2m,
+    :time,
     :temperature_2m,
     :weather_code,
     :wind_speed_10m
@@ -28,11 +32,13 @@ defmodule WeatherGraphqlGateway.OpenMeteoAPI.Models.CurrentWeather do
 
   @type t :: %__MODULE__{
     apparent_temperature: number(),
-    is_day: number(),
-    precipitation: number(),
-    relative_humidity_2m: number(),
+    cloud_cover: integer(),
+    is_day: integer(),
+    precipitation: integer(),
+    relative_humidity_2m: integer(),
+    time: String.t(),
     temperature_2m: number(),
-    weather_code: number(),
-    wind_speed_10m: number()
+    weather_code: integer(),
+    wind_speed_10m: integer()
   }
 end
