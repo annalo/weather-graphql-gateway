7fe23f4dce3e0d19473e53a2fcec7546c6ce2a1d
Anna Lo <annayylo@gmail.com>
2024-04-04 16:13:49 -0400

    remove precipitation sum from daily weather

--- lib/weather_graphql_gateway/open-meteo-api/models/daily_weather.ex
+++ lib/weather_graphql_gateway/open-meteo-api/models/daily_weather.ex
@@ -7,7 +7,6 @@ defmodule WeatherGraphqlGateway.OpenMeteoAPI.Models.DailyWeather do
     ## Fields
 
     - `precipitation_probability_max`: The max precipitation probability (in percentage).
-    - `precipitation_sum`: Total daily precipitation (rain, showers, snow). (mm/inch)
     - `sunrise`: The time of sunrise. (in local timezone)
     - `sunset`: The time of sunset. (in local timezone)
     - `temperature_2m_max`: The maximum air temperature at 2 meters above ground level. (C/F)
@@ -17,7 +16,6 @@ defmodule WeatherGraphqlGateway.OpenMeteoAPI.Models.DailyWeather do
   """
   defstruct [
     :precipitation_probability_max,
-    :precipitation_sum,
     :sunrise,
     :sunset,
     :temperature_2m_max,
@@ -27,8 +25,7 @@ defmodule WeatherGraphqlGateway.OpenMeteoAPI.Models.DailyWeather do
   ]
 
   @type t :: %__MODULE__{
-          precipitation_probability_max: [number()],
-          precipitation_sum: [number()],
+          precipitation_probability_max: [integer()],
           sunrise: [String.t()],
           sunset: [String.t()],
           temperature_2m_max: [number()],
