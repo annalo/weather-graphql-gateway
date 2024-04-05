64355b53471fe83e3eb859af922d8e6873cca05c
Anna Lo <annayylo@gmail.com>
2024-04-05 15:43:20 -0400

    add handle_response

--- lib/weather_graphql_gateway/open-meteo-api/client.ex
+++ lib/weather_graphql_gateway/open-meteo-api/client.ex
@@ -12,13 +12,20 @@ defmodule WeatherGraphqlGateway.OpenMeteoAPI.Client do
   def get_weather(%WeatherRequest{} = request) do
     request
     |> build_url()
-    |> Req.get!()
+    |> Req.get()
+    |> handle_response()
   end
 
+  @spec build_url(WeatherGraphqlGateway.OpenMeteoAPI.Models.WeatherRequest.t()) :: String.t()
   def build_url(%WeatherRequest{} = request) do
     params = params_without_nil(request)
     @base_url <> "?" <> Query.encode(params)
   end
+
+  @spec handle_response({:ok, Req.Response.t()}) :: %{}
+  @spec handle_response({:error, Exception.t()}) :: Exception.t()
+  def handle_response({:ok, response}), do: response.body
+  def handle_response({:error, exception}), do: exception
 
   # remove keys with nil value from params
   defp params_without_nil(%WeatherRequest{} = struct) do
--- test/weather_graphql_gateway/open-meteo-api/client_test.exs
+++ test/weather_graphql_gateway/open-meteo-api/client_test.exs
@@ -6,28 +6,40 @@ defmodule WeatherGraphqlGateway.OpenMeteoAPI.ClientTest do
 
   describe "Client.get_weather" do
     test "gets weather data for a valid request" do
-      request = %WeatherRequest{latitude: 35.8317, longitude: -78.9286}
-      {:ok, response} = Client.get_weather(request)
+      request = %WeatherRequest{
+        latitude: 35.8317,
+        longitude: -78.9286,
+        current: ["temperature_2m"],
+        daily: ["weather_code"],
+        hourly: ["precipitation_probability"],
+        forecast_days: 1
+      }
+      response = Client.get_weather(request)
 
-      # Assert
-      assert Map.has_key?(response, :current_weather)
-      assert Map.has_key?(response, :daily)
+      assert response["latitude"] == 35.823345
+      assert response["longitude"] == -78.93414
+      assert response["timezone"] == "America/New_York"
+      assert response["timezone_abbreviation"] == "EDT"
+
+      assert response["current"]["temperature_2m"] |> is_number() == true
+      assert response["hourly"]["precipitation_probability"] |> length() == 24
+      assert response["daily"]["weather_code"] |> length() == 1
     end
   end
 
   describe "Client.build_url" do
     test "with all parameters" do
       request = %WeatherRequest{
-        current: ["temperature_2m"],
-        daily: ["weather_code", "temperature_2m_max", "temperature_2m_min", "sunrise", "sunset", "precipitation_probability_max"],
-        forecast_days: 3,
-        hourly: ["temperature_2m", "precipitation_probability", "weather_code", "is_day"],
         latitude: 35.7326,
         longitude: 78.8503,
+        current: ["temperature_2m"],
+        daily: ["weather_code", "temperature_2m_max", "temperature_2m_min", "sunrise", "sunset", "precipitation_probability_max"],
+        hourly: ["temperature_2m", "precipitation_probability", "weather_code", "is_day"],
         precipitation_unit: "inch",
         temperature_unit: "fahrenheit",
-        timezone: "America/New_York",
         wind_speed_unit: "mph",
+        timezone: "America/New_York",
+        forecast_days: 3
       }
 
       uri = Client.build_url(request) |> URI.parse()
@@ -47,9 +59,9 @@ defmodule WeatherGraphqlGateway.OpenMeteoAPI.ClientTest do
 
     test "removes keys with nil value" do
       request = %WeatherRequest{
-        current: ["temperature_2m"],
         latitude: 35.7326,
         longitude: -78.8503,
+        current: ["temperature_2m"],
         temperature_unit: "fahrenheit"
       }
 
