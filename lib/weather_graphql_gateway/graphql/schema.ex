e2cfb810eb2237e84e86f7177b273cd3228f39db
Anna Lo <annayylo@gmail.com>
2024-04-08 13:03:08 -0400

    fix schema

--- lib/weather_graphql_gateway/graphql/schema.ex
+++ lib/weather_graphql_gateway/graphql/schema.ex
@@ -70,26 +70,33 @@ defmodule WeatherGraphqlGateway.Schema do
     field :time, :string
     @desc "Weather condition as a numeric code. Follow WMO weather interpretation codes."
     field :weather_code, :integer
+  end
 
+  object :weather do
+    @desc "Daily weather data for the specified `forecast_days` field. Defaults to 7 days."
+    field :daily, list_of(:daily) do
+      @desc "Per default, only 7 days are returned. Up to 16 days of forecast are possible."
+      arg :days, :integer
+    end
+    @desc "Hourly data for the day (24h)."
+    field :hourly, list_of(:hourly) do
+      @desc "Per default, the next 24 hours are returned. Up to 16 days of forecast are possible."
+      arg :hours, :integer
+    end
+    @desc "Current weather data."
+    field :current, :current
   end
 
   query do
-    field :latitude, non_null(:float)
-    field :longitude, non_null(:float)
-
-    @desc "The number of days to include in the daily forecast. Defaults to 7."
-    field :forecast_days, :integer
-    @desc "The unit of measurement for precipitation. Defaults to mm. (mm/inch)"
-    field :precipitation_unit, :precipitation_unit
-    @desc "The unit of measurement for temperature. Defaults to Celsius. (celsius/fahrenheit)"
-    field :temperature_unit, :temperature_unit
-    @desc "The unit of measurement for wind speed. Defaults to kmh. (kmh, ms, mph, kn)"
-    field :wind_speed_unit, :wind_speed_unit
-    @desc "Daily weather data for the specified `forecast_days` field. Defaults to 7 days."
-    field :daily, list_of(:daily)
-    @desc "Hourly data for the day (24h)."
-    field :hourly, list_of(:hourly)
-    @desc "Current weather data."
-    field :current, :current
+    field :weather, :weather do
+      arg :latitude, non_null(:float)
+      arg :longitude, non_null(:float)
+      @desc "The unit of measurement for precipitation. Defaults to mm. (mm/inch)"
+      arg :precipitation_unit, :precipitation_unit
+      @desc "The unit of measurement for temperature. Defaults to Celsius. (celsius/fahrenheit)"
+      arg :temperature_unit, :temperature_unit
+      @desc "The unit of measurement for wind speed. Defaults to kmh. (kmh, ms, mph, kn)"
+      arg :wind_speed_unit, :wind_speed_unit
+    end
   end
 end
