3ab2822ea4c6feab5c6698ec3351d21b4509b26d
Anna Lo <annayylo@gmail.com>
2024-04-15 13:43:24 -0400

    do not set a default limit or a default language

--- lib/weather_graphql_gateway/nominatim-api/client.ex
+++ lib/weather_graphql_gateway/nominatim-api/client.ex
@@ -7,10 +7,15 @@ defmodule WeatherGraphqlGateway.NominatimAPI.Client do
   can be used to make requests to the API and retrieve geocoding information.
   """
   alias WeatherGraphqlGateway.NominatimAPI.Models.Geocode
-
+  require Logger
   # https://nominatim.openstreetmap.org/search?<params>
   @geocode_url "https://nominatim.openstreetmap.org/search?format=jsonv2"
   @reverse_url "https://nominatim.openstreetmap.org/reverse?format=jsonv2"
+
+  # zoom: Level of detail required for the address
+  # https://nominatim.org/release-docs/develop/api/Reverse/#example-with-formatjsonv2
+  # set to zoom to neighborhood
+  @zoom_level 10
 
   import Plug.Conn.Query
 
@@ -30,11 +35,18 @@ defmodule WeatherGraphqlGateway.NominatimAPI.Client do
   A map containing the geocoding information retrieved from the Nominatim API, or an exception if the request fails. The returned map may include keys like `lat`, `lon`, and other details depending on the API response.
   """
   @spec geocode(String.t()) :: [map()]
-  def geocode(
-        query,
-        limit \\ 10,
-        language \\ "en"
-      ) do
+  def geocode(query) do
+    query
+    |> build_geocode_url()
+    |> Req.get()
+    |> handle_response()
+  end
+
+  @spec geocode(String.t(), %{
+          language: String.t() | nil,
+          limit: number() | nil
+        }) :: [map()]
+  def geocode(query, %{language: language, limit: limit}) do
     query
     |> build_geocode_url(limit, language)
     |> Req.get()
@@ -49,48 +61,74 @@ defmodule WeatherGraphqlGateway.NominatimAPI.Client do
   # Arguments
 
     - `coordinates` - A map with :lat and :lon keys representing the geographical coordinates to be reverse geocoded.
+    - `language` - "Preferred language order for showing search results. This may either be a simple comma-separated list of language codes. Defaults to 'en'."
 
   # Returns
 
   A map containing the address details retrieved from the Nominatim API based on the provided coordinate.
   """
-  @spec reverse_geocode(
-          %{lat: number(), lon: number()},
-          String.t()
-        ) :: map()
-  def reverse_geocode(
-        %{lat: lat, lon: lon},
-        language \\ "en"
-      ) do
+  @spec reverse_geocode(number(), number()) :: map()
+  def reverse_geocode(lat, lon) do
+    build_reverse_url(lat, lon)
+    |> Req.get()
+    |> handle_response()
+  end
+
+  @spec reverse_geocode(number(), number(), String.t() | nil) :: map()
+  def reverse_geocode(lat, lon, language) do
     build_reverse_url(lat, lon, language)
     |> Req.get()
     |> handle_response()
   end
 
-  @spec build_geocode_url(String.t(), integer(), String.t()) :: String.t()
+  @spec build_geocode_url(String.t()) :: String.t()
+  defp build_geocode_url(query), do: @geocode_url <> "&" <> encode(q: query)
+
+  @spec build_geocode_url(String.t(), integer() | nil, String.t() | nil) :: String.t()
   defp build_geocode_url(query, limit, language) do
-    @geocode_url <>
-      "&" <>
-      encode(
-        q: query,
-        limit: limit,
-        "accept-language": language
-      )
+    case {limit, language} do
+      {nil, nil} ->
+        @geocode_url <> "&" <> encode(q: query)
+
+      {nil, language} ->
+        @geocode_url <> "&" <> encode(q: query, "accept-language": language)
+
+      {limit, nil} ->
+        @geocode_url <> "&" <> encode(q: query, limit: limit)
+
+      {limit, language} ->
+        @geocode_url <> "&" <> encode(q: query, "accept-language": language, limit: limit)
+    end
+  end
+
+  @spec build_reverse_url(number(), number()) :: String.t()
+  defp build_reverse_url(lat, lon) do
+    @reverse_url <> "&" <> encode_reverse_url_params(lat, lon, nil)
+  end
+
+  @spec build_reverse_url(number(), number(), nil) :: String.t()
+  defp build_reverse_url(lat, lon, nil) do
+    @reverse_url <> "&" <> encode_reverse_url_params(lat, lon, nil)
   end
 
   @spec build_reverse_url(number(), number(), String.t()) :: String.t()
   defp build_reverse_url(lat, lon, language) do
-    # zoom: Level of detail required for the address
-    # https://nominatim.org/release-docs/develop/api/Reverse/#example-with-formatjsonv2
-    # set to zoom to neighborhood
-    @reverse_url <>
-      "&" <>
-      encode(
-        lat: lat,
-        lon: lon,
-        "accept-language": language,
-        zoom: 10
-      )
+    @reverse_url <> "&" <> encode_reverse_url_params(lat, lon, language)
+  end
+
+  @spec encode_reverse_url_params(number(), number(), nil) :: String.t()
+  defp encode_reverse_url_params(lat, lon, nil) do
+    encode(lat: lat, lon: lon, zoom: @zoom_level)
+  end
+
+  @spec encode_reverse_url_params(number(), number(), String.t()) :: String.t()
+  defp encode_reverse_url_params(lat, lon, language) do
+    encode(
+      lat: lat,
+      lon: lon,
+      "accept-language": language,
+      zoom: @zoom_level
+    )
   end
 
   @spec handle_response({:ok, Req.Response.t()}) :: %{}
@@ -105,6 +143,8 @@ defmodule WeatherGraphqlGateway.NominatimAPI.Client do
 
   @spec deserialize_response(map()) :: Geocode.t()
   defp deserialize_response(object) do
+    # Logger.debug("#{object}")
+
     %Geocode{
       boundingbox: object["boundingbox"] |> deserialize_boundingbox(),
       display_name: object["display_name"],
--- test/weather_graphql_gateway/nominatim-api/client_test.exs
+++ test/weather_graphql_gateway/nominatim-api/client_test.exs
@@ -11,29 +11,55 @@ defmodule WeatherGraphqlGateway.NominatimAPI.ClientTest do
       assert first.boundingbox == [55.5267243, 55.8467243, 12.4100724, 12.7300724]
 
       assert first.display_name ==
-               "Copenhagen, Copenhagen Municipality, Capital Region of Denmark, 1357, Denmark"
+               "København, Københavns Kommune, Region Hovedstaden, 1357, Danmark"
 
       assert first.lat == 55.6867243
       assert first.lon == 12.5700724
-      assert first.name == "Copenhagen"
+      assert first.name == "København"
       assert first.place_rank == 15
+    end
+
+    test "sets limit parameter" do
+      response = Client.geocode("Copenhagen", %{limit: 2, language: nil})
+      assert length(response) == 2
+    end
+
+    test "sets language parameter" do
+      response = Client.geocode("Taipei", %{limit: 1, language: "zh-TW"})
+      first = Enum.at(response, 0)
+
+      assert first.display_name == "臺北市, 臺灣"
+      assert first.name == "臺北市"
     end
   end
 
   describe "Client.reverse_geocode/1" do
     test "reverse geocoding" do
       response =
-        Client.reverse_geocode(%{
-          lat: 25.0330,
-          lon: 121.5654
-        })
+        Client.reverse_geocode(25.0330, 121.5654)
 
       assert response.boundingbox == [25.0081697, 25.0495075, 121.5526148, 121.5922279]
-      assert response.display_name == "Xinyi District, Taipei, Taiwan"
+      assert response.display_name == "信義區, 臺北市, 臺灣"
       assert response.lat == 25.0333448
       assert response.lon == 121.5668963
-      assert response.name == "Xinyi District"
+      assert response.name == "信義區"
       assert response.place_rank == 14
+    end
+
+    test "sets language parameter" do
+      response =
+        Client.reverse_geocode(25.0330, 121.5654, "en")
+
+      assert response.display_name == "Xinyi District, Taipei, Taiwan"
+      assert response.name == "Xinyi District"
+    end
+
+    test "returns locale language if language is set to nil" do
+      response =
+        Client.reverse_geocode(25.0330, 121.5654, nil)
+
+      assert response.display_name == "信義區, 臺北市, 臺灣"
+      assert response.name == "信義區"
     end
   end
 end
